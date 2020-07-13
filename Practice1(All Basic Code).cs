96d9be7c5967e438070b700a35698b999c9984a8
Hitesh Jangid <hiten2112@users.noreply.github.com>
2020-07-13 08:36:58 +0530

    Add files via upload

---
+++ Practice1(All Basic Code).cs
@@ -0,0 +1,137 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using excel = Microsoft.Office.Interop.Excel;
+
+namespace SelAssignments
+{
+    [TestFixture]
+    public class Practice1
+    {
+        IWebDriver driver;
+        string url = "http://10.82.180.36/Common/Login.aspx";
+        [SetUp]
+        public void SeUpMethod()
+        {
+            driver = new ChromeDriver(@"C:\IVS Files\Selenium\Drivers\recentDrivers\chromedriver_win32_2.43");
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+        }
+        [Test]
+        public void TestMethod()
+        {
+            //Login
+            driver.FindElement(By.Id("body_txtUserID")).SendKeys("donhere");
+            driver.FindElement(By.Id("body_txtPassword")).SendKeys("don@123");
+            driver.FindElement(By.Id("body_btnLogin")).Click();
+            Thread.Sleep(2000);
+
+            //Fetch the "Welcome Donhere" text
+            String text = driver.FindElement(By.XPath("/html/body/form/div[3]/div[2]")).Text;
+            Console.WriteLine(text);
+            Thread.Sleep(2000);
+
+            //Table Data
+           
+            IWebElement table = driver.FindElement(By.Id("body_cph_MyAccount_gvAccountDetails"));
+            IList<IWebElement> table_rows = table.FindElements(By.TagName("tr"));
+            int size_rows = table_rows.Count;
+           
+            for(int i=1;i<=size_rows-3;i++)
+            {
+               IList<IWebElement> table_cols = table_rows[i].FindElements(By.TagName("td"));
+               int size_col = table_cols.Count;
+               
+                    if(table_cols[3].Text=="Current Account")
+                    {
+                        Console.WriteLine("Account Number : "+table_cols[0].Text+"\t\t"+"Balance : "+table_cols[1].Text);
+                    }
+                
+
+            }
+            Thread.Sleep(2000);
+            //Mouse Hovering
+            Actions action = new Actions(driver);
+            IWebElement ChequeBookButton = driver.FindElement(By.XPath("/html/body/form/div[3]/div[4]/div[1]/div/div/ul/li[6]/a"));
+            action.MoveToElement(ChequeBookButton).Perform();
+
+            //Implicit Wait
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            //Click on the button appear  after hovering
+            driver.FindElement(By.XPath("/html/body/form/div[3]/div[4]/div[1]/div/div/ul/li[6]/ul/li[2]/a")).Click();
+            Thread.Sleep(2000);
+
+            //DropDown and Printing whole DropDown element
+            IWebElement formsrc = driver.FindElement(By.Id("body_cph_MyAccount_ddlStatusType"));
+            SelectElement selectFrom = new SelectElement(formsrc);
+            IList<IWebElement> alloptions = selectFrom.Options;
+
+            int size = alloptions.Count;
+            for (int i = 1; i <= size - 1; i++)
+            {
+                Console.WriteLine(alloptions[i].Text);
+            }
+
+            //Click on Loans
+            driver.FindElement(By.Id("GeneralTabMenu_Loans_li_Cust")).Click();
+
+            //Reading data from excelsheet
+            excel.Application x1app = new excel.Application(); //creating instance of excel app
+            excel.Workbook x1workbook = x1app.Workbooks.Open(@"C:\Users\hitesh.jangid01\Desktop\EMI.xlsx"); //getting the actual excel file
+            excel._Worksheet x1worksheet = x1workbook.Sheets[1]; //Getting sheet from where data will being fetched "here data in sheet 1"
+            excel.Range x1range = x1worksheet.UsedRange; //All the data available
+
+            int rowcount = x1range.Rows.Count;
+            Console.WriteLine(rowcount);
+
+            for (int i = 2; i <= rowcount; i++)
+            {
+                IWebElement toSelect1 = driver.FindElement(By.Id("body_cph_Loans_ddlLoanType"));
+                SelectElement oSelect1 = new SelectElement(toSelect1);
+                oSelect1.SelectByText(x1range.Cells[i,1].Value2);
+                Thread.Sleep(2000);
+
+                IWebElement toSelect2 = driver.FindElement(By.Id("body_cph_Loans_ddlLoanName"));
+                SelectElement oSelect2 = new SelectElement(toSelect2);
+                oSelect2.SelectByText(x1range.Cells[i, 2].Value2);
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.Id("body_cph_Loans_txtReqLoanAmount")).SendKeys("" + x1range.Cells[i, 3].Value2);
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.Id("body_cph_Loans_txtNoOfEMI")).SendKeys("" + x1range.Cells[i, 4].Value2);
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.Id("body_cph_Loans_btnViewEMI")).Click();
+                Thread.Sleep(2000);
+
+                String emi_amount = driver.FindElement(By.Id("body_cph_Loans_lblEMIAmountText")).Text;
+
+                //write back to excel file
+                x1range.Cells[i, 5] = emi_amount;
+            }
+            x1workbook.SaveAs("EMiEdited.xlsx");
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("About Us")).Click();
+            Thread.Sleep(2000);
+        }
+
+
+        [TearDown]
+        public void TearDownMethod()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ TestClass1.cs
@@ -0,0 +1,253 @@
+﻿using Microsoft.Office.Interop.Excel;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+//log4net to be configured using the App.config xml file
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace assignment_1075913
+{
+	[TestFixture]
+	public class TestClass1
+	{
+		//setting up the log4net to access its methods
+		private log4net.ILog log = log4net.LogManager.GetLogger(typeof(TestClass1));
+
+		private IWebDriver driver;
+		private string url = "http://10.82.180.36:81/Automation/MobileRecharge/#/home";
+
+
+		[OneTimeSetUp]
+		public void Setup()
+		{
+			log.Info("inside setup method");
+			driver = new ChromeDriver(@"C:\IVS Files\Selenium\Drivers\recentDrivers\chromedriver_win32_2.43");
+			driver.Navigate().GoToUrl(url);
+			driver.Manage().Window.Maximize();
+
+			//implicit wait
+			driver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(50);
+
+			//page title
+			string title = driver.Title;
+			log.Info(title);
+
+
+
+
+
+
+
+		}
+
+
+		[Test]
+		public void TestMethod()
+		{
+			log.Info("inside test method");
+			driver.FindElement(By.LinkText("Login")).Click();
+
+			driver.FindElement(By.Id("username")).SendKeys("Tony");
+			driver.FindElement(By.Id("password")).SendKeys("TonyStark@123");
+			driver.FindElement(By.XPath("/html/body/div[1]/div/div/form/div[3]/input")).Click();
+			Thread.Sleep(7000);
+			driver.FindElement(By.LinkText("DTH")).Click();
+			Thread.Sleep(7000);
+			driver.FindElement(By.XPath("/html/body/nav/ul[1]/li[3]/ul/li/a")).Click();
+			Thread.Sleep(7000);
+
+			//for drop down
+			IWebElement select_city = driver.FindElement(By.XPath("//*[@id='back']/center/form/div[1]/select"));
+			SelectElement city = new SelectElement(select_city);
+			//IList<IWebElement> city_ddl = city.Options;
+			city.SelectByValue("Bengluru");
+			Thread.Sleep(7000);
+
+			IWebElement location = driver.FindElement(By.XPath("//*[@id='back']/center/form/div[2]/select"));
+			SelectElement loc = new SelectElement(location);
+			loc.SelectByValue("Electronics City");
+			Thread.Sleep(7000);
+
+			//radio button
+			driver.FindElement(By.XPath("//*[@id='back']/center/form/div[3]/input[1]")).Click();
+			Thread.Sleep(7000);
+
+			driver.FindElement(By.XPath("//*[@id='back']/center/form/div[4]/div/input")).Clear();
+			Thread.Sleep(7000);
+
+			driver.FindElement(By.XPath("//*[@id='back']/center/form/div[4]/div/input")).SendKeys("1");
+			Thread.Sleep(7000);
+
+
+			driver.FindElement(By.XPath("//*[@id='back']/center/form/div[5]/textarea")).SendKeys("Begur Hobli");
+			///explicit wait
+			WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromMilliseconds(5000));
+
+			driver.FindElement(By.XPath("//*[@id='back']/center/form/center/a")).Click();
+			Thread.Sleep(7000);
+
+			driver.FindElement(By.Id("wallet")).Click();
+			Thread.Sleep(7000);
+			driver.FindElement(By.XPath("//*[@id='quick']")).Click();
+			Thread.Sleep(7000);
+
+			//print meaasge apeear on screen
+			string message = driver.FindElement(By.XPath("/html/body/div[5]/p")).Text;
+			Console.WriteLine(message);
+
+
+			driver.FindElement(By.XPath("/ html / body / div[5] / div[7] / div / button")).Click();
+			Thread.Sleep(7000);
+
+			driver.FindElement(By.XPath("//*[@id='success']/center/a[1]")).Click();
+			Thread.Sleep(7000);
+
+			driver.FindElement(By.Id("post")).SendKeys("8877998765");
+			Thread.Sleep(7000);
+
+			driver.FindElement(By.Id("roambsnl")).Click();
+			Thread.Sleep(7000);
+			string message1 = driver.FindElement(By.XPath("/ html / body / div[1] / div / div / div[2]")).Text;
+			Console.WriteLine(message1);
+
+
+			driver.FindElement(By.XPath("/html/body/div[1]/div/div/div[3]/button")).Click();
+			Thread.Sleep(7000);
+
+			driver.FindElement(By.Id("airteltopup")).Click();
+			Thread.Sleep(7000);
+
+			///table 
+			IWebElement table = driver.FindElement(By.XPath("//*[@id='airtelto']"));
+			IList<IWebElement> table_rows = table.FindElements(By.TagName("tr"));
+			foreach (IWebElement row in table_rows)
+			{
+				IList<IWebElement> cols = row.FindElements(By.TagName("td"));
+				foreach (IWebElement col in cols)
+				{
+					Console.WriteLine(col.Text);
+				}
+
+
+			}
+			driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/button")).Click();
+			Thread.Sleep(7000);
+			//log.Error("cancel");
+
+			driver.FindElement(By.XPath("/html/body/nav/ul[1]/li[2]/a")).Click();
+			Thread.Sleep(7000);
+
+			log.Error("postpaid in dropdown is not clickable");
+			Thread.Sleep(9000);
+			//excel file for input
+			string excelpath = @"Z:\Data.xlsx";
+			Application excelapp;
+			Workbook wbook;
+			Worksheet wsheet;
+			excelapp = new Application();
+
+			wbook = excelapp.Workbooks.Open(excelpath);
+			wsheet = wbook.Worksheets["postpaid"];
+			excelapp.Visible = true;
+
+			int rowcount = wsheet.UsedRange.Rows.Count;
+			//Console.WriteLine(rowcount);
+			//int coloumncount = wsheet.UsedRange.Rows.Count;
+			///Console.WriteLine(coloumncount);
+
+			for (int i = 2; i <= rowcount; i++)
+			{
+				driver.FindElement(By.Id("post")).SendKeys(wsheet.Cells[i, 1].Text);
+				Thread.Sleep(2000);
+				driver.FindElement(By.Name("opernum")).SendKeys(wsheet.Cells[i, 2].Text);
+				Thread.Sleep(2000);
+				driver.FindElement(By.Name("posamo")).Clear();
+				Thread.Sleep(2000);
+				driver.FindElement(By.Name("posamo")).SendKeys(wsheet.Cells[i, 3].Text);
+				Thread.Sleep(2000);
+				driver.FindElement(By.XPath("//*[@id='postv']/form/center/a")).Click();
+				Thread.Sleep(2000);
+
+				driver.FindElement(By.Id("debit")).Click();
+				Thread.Sleep(2000);
+				driver.FindElement(By.Id("in1")).SendKeys("1234");
+				Thread.Sleep(2000);
+				driver.FindElement(By.Id("in2")).SendKeys("2345");
+				Thread.Sleep(2000);
+				driver.FindElement(By.Id("in3")).SendKeys("3456");
+				Thread.Sleep(2000);
+				driver.FindElement(By.Id("in4")).SendKeys("9456");
+				Thread.Sleep(2000);
+
+
+				//dropdown
+				IWebElement month = driver.FindElement(By.Id("month"));
+				SelectElement mon = new SelectElement(month);
+				mon.SelectByValue("Feb");
+				Thread.Sleep(2000);
+
+				//dropdown
+				IWebElement year = driver.FindElement(By.Id("year"));
+				SelectElement yr = new SelectElement(year);
+				yr.SelectByValue("2018");
+				Thread.Sleep(2000);
+
+				driver.FindElement(By.Name("cardCVV")).SendKeys("123");
+				Thread.Sleep(2000);
+
+				driver.FindElement(By.Id("confirm-purchase")).Click();
+				Thread.Sleep(2000);
+
+
+				driver.FindElement(By.XPath("/html/body/div[5]/div[7]/div/button")).Click();
+				//screen shot
+				Screenshot scrfile = ((ITakesScreenshot)driver).GetScreenshot();
+				scrfile.SaveAsFile(@"C:\Users\apurva.tatekar\source\repos\image "+i+".png");
+
+				Thread.Sleep(6000);
+
+				///excel writeback
+				string message2 = driver.FindElement(By.Id("successmessage")).Text;
+				Thread.Sleep(2000);
+				wsheet.Cells[i, 4] = message2;
+				Thread.Sleep(6000);
+
+
+				driver.FindElement(By.XPath("//*[@id='success']/center/a[2]")).Click();
+				Thread.Sleep(2000);
+			}
+
+			wbook.Save();
+			wbook.Close();
+			excelapp.Quit();
+			Thread.Sleep(2000);
+
+
+
+
+
+
+
+		}
+
+
+
+
+		[OneTimeTearDown]
+		public void Teardown()
+		{
+			log.Info("inside tear down");
+			driver.Close();
+			driver.Quit();
+		}
+	}
+}
